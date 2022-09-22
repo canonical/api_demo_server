1895a9693fe5a3f6df6eb1065ecc2c0647b8a8c9
Maksim Beliaev <beliaev.m.s@gmail.com>
2022-09-22 17:35:56 +0200

    move db

--- api_demo_server/app.py
+++ api_demo_server/app.py
@@ -1,57 +1,7 @@
 from starlette_exporter import PrometheusMiddleware, handle_metrics
 from fastapi import FastAPI, Form
-import psycopg2
-from psycopg2 import sql
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-
-class DataBase:
-    def __init__(self):
-        self.conn = None
-        self._cursor = None
-
-    @property
-    def connected(self):
-        return self.conn is not None
-
-    @property
-    def cursor(self):
-        if not self.connected:
-            self.connect_to_postgres()
-
-        if self._cursor is None:
-            self._cursor = self.conn.cursor()
-
-        return self._cursor
-
-    def db_exists(self, name):
-        self.cursor.execute("select exists(select * from pg_database where datname=%s)", (name,))
-        if self.cursor.fetchone()[0]:
-            print(f"Database {name} already exists.")
-            return True
-        return False
-
-    def connect_to_postgres(self):
-        if not self.connected:
-            self.conn = psycopg2.connect(user="postgres", password="mysecretpassword", host="127.0.0.1", port="5432")
-            self.conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-            print("connected successfully")
-
-    def create_db(self, db_name):
-        if not self.db_exists(db_name):
-            # Prevent sql injection attack by using sql module instead of string concat
-            self.cursor.execute(sql.SQL("CREATE DATABASE {}").format(
-                sql.Identifier(db_name))
-            )
-
-    def drop_db(self, db_name):
-        # Prevent sql injection attack by using sql module instead of string concat
-        self.cursor.execute(sql.SQL("DROP DATABASE IF EXISTS {}").format(
-            sql.Identifier(db_name))
-        )
-        if not self.db_exists(db_name):
-            print(f"Database {db_name} was successfully removed")
-
+from api_demo_server.database import DataBase
 
 PSQL_DB = DataBase()
 
