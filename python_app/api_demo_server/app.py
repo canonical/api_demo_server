5136ec55d9a2ed9cb33696b92f2fbf45a251a272
Maksim Beliaev <beliaev.m.s@gmail.com>
2022-09-30 13:02:49 +0200

    App upd (#5)
    * remove connect end point
    * bump version
    * add redirect
    * wrap logger definition
    * reworked DB

--- python_app/api_demo_server/app.py
+++ python_app/api_demo_server/app.py
@@ -3,6 +3,7 @@ import os
 
 from fastapi import FastAPI
 from fastapi import Form
+from starlette.responses import RedirectResponse
 from starlette.responses import Response
 from starlette_exporter import PrometheusMiddleware
 from starlette_exporter import handle_metrics
@@ -10,22 +11,26 @@ from starlette_exporter import handle_metrics
 from . import __version__
 from .database import DataBase
 
-log_file = os.environ.get("DEMO_SERVER_LOGFILE", "demo_server.log")
 
-logger = logging.getLogger("api-demo-server")
-logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    fmt="%(asctime)s (%(levelname)s) %(message)s", datefmt="%d.%m.%Y %H:%M:%S"
-)
+def define_logger():
+    """Define logger to output to the file and to STDOUT."""
+    log = logging.getLogger("api-demo-server")
+    log.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        fmt="%(asctime)s (%(levelname)s) %(message)s", datefmt="%d.%m.%Y %H:%M:%S"
+    )
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    log.addHandler(stream_handler)
 
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
-logger.addHandler(stream_handler)
+    log_file = os.environ.get("DEMO_SERVER_LOGFILE", "demo_server.log")
+    file_handler = logging.FileHandler(filename=log_file)
+    file_handler.setFormatter(formatter)
+    log.addHandler(file_handler)
+    return log
 
-file_handler = logging.FileHandler(filename=log_file)
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
 
+logger = define_logger()
 
 PSQL_DB = DataBase()
 
@@ -35,6 +40,11 @@ app.add_route("/metrics", handle_metrics)
 
 
 async def catch_exceptions_middleware(request, call_next):
+    """Middleware to catch all exceptions.
+
+    All exceptions that were raised during handling of the request will be caught
+    and logged with the traceback, then 500 response will be returned to the user.
+    """
     try:
         return await call_next(request)
     except Exception:
@@ -47,12 +57,8 @@ app.middleware("http")(catch_exceptions_middleware)
 
 @app.get("/")
 def root():
-    return {"message": "Demo API server with prometheus endpoint"}
-
-
-@app.post("/connectdb")
-def connect_to_database():
-    PSQL_DB.connect_to_psql()
+    """Just redirect from root path to Swagger UI"""
+    return RedirectResponse(url="/docs")
 
 
 @app.post("/dropdb")
@@ -62,19 +68,18 @@ def drop_database():
 
 @app.post("/createtable")
 def create_table():
-    PSQL_DB.create_table()
+    PSQL_DB.create_table(db_name="names_db", table_name="names")
 
 
 @app.post("/addname/")
 def add_name(username: str = Form()):
-    PSQL_DB.add_name(username)
+    PSQL_DB.add_name(username, db_name="names_db", table_name="names")
     return {"username": username}
 
 
 @app.get("/names")
 def get_all_names():
-
-    return {"names": dict(PSQL_DB.all_names)}
+    return {"names": dict(PSQL_DB.all_names(db_name="names_db", table_name="names"))}
 
 
 @app.get("/error")
--- python_app/api_demo_server/database.py
+++ python_app/api_demo_server/database.py
@@ -1,5 +1,7 @@
 import logging
 import os
+from typing import List
+from typing import Tuple
 
 import psycopg2
 from psycopg2 import sql
@@ -16,32 +18,35 @@ DB_PASSWORD = os.environ.get("DEMO_SERVER_DB_PASSWORD", "mysecretpassword")
 
 
 class DataBase:
-    def __init__(self):
+    def __init__(self) -> None:
         self.psql_conn = None
         self.psql_cursor = None
         self.db_conn = None
         self.db_cursor = None
 
-    def connect_to_psql(self):
+    def connect_to_psql(self) -> None:
+        """Connects to PSQL without specification of DB"""
         self.psql_conn = psycopg2.connect(
             user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT
         )
         self.psql_conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
         self.psql_cursor = self.psql_conn.cursor()
 
-    def db_exists(self, name):
+    def db_exists(self, db_name: str) -> bool:
+        """Checks if DB already defined in PSQL."""
         if self.psql_conn is None:
             self.connect_to_psql()
 
         self.psql_cursor.execute(
-            "select exists(select * from pg_database where datname=%s)", (name,)
+            "select exists(select * from pg_database where datname=%s)", (db_name,)
         )
         if self.psql_cursor.fetchone()[0]:
-            logger.info(f"Database '{name}' already exists.")
+            logger.info(f"Database '{db_name}' already exists.")
             return True
         return False
 
-    def connect_to_db(self, db_name):
+    def connect_to_db(self, db_name: str) -> None:
+        """Connects to the database, creates if it does not exist."""
         self.create_db(db_name)
 
         self.db_conn = psycopg2.connect(
@@ -55,23 +60,39 @@ class DataBase:
 
         logger.info(f"Successfully connected to database: {db_name}")
 
-    def create_db(self, db_name):
+    def close_all_db_connections(self, db_name: str) -> None:
+        """Close all the connections to the database.
+
+        This might be required to perform some operations on DB, like drop"""
+        self.psql_cursor.execute(
+            """
+            SELECT pg_terminate_backend(pg_stat_activity.pid)
+            FROM pg_stat_activity
+            WHERE pg_stat_activity.datname = %s;""",
+            (db_name,),
+        )
+        logger.info(f"All connections to DB '{db_name}' were closed.")
+
+    def create_db(self, db_name: str) -> None:
+        """Creates database if it does not exist."""
         if not self.db_exists(db_name):
             # Prevent sql injection attack by using sql module instead of string concat
             self.psql_cursor.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(db_name)))
+            logger.info(f"Database '{db_name}' was created.")
 
-    def drop_db(self, db_name):
+    def drop_db(self, db_name: str) -> None:
+        """Drops (deletes) database if exists"""
         if self.db_exists(db_name):
-            # Prevent sql injection attack by using sql module instead of string concat
-            self.psql_cursor.execute(
-                sql.SQL("DROP DATABASE IF EXISTS {}").format(sql.Identifier(db_name))
-            )
+            self.close_all_db_connections(db_name)
+            self.psql_cursor.execute(sql.SQL("DROP DATABASE {}").format(sql.Identifier(db_name)))
         if not self.db_exists(db_name):
             self.db_conn = None
             self.db_cursor = None
             logger.info(f"Database '{db_name}' was successfully removed")
 
-    def table_exists(self, table_name):
+    def _table_exists(self, table_name: str) -> bool:
+        """Checks if the table already exists."""
+        assert self.db_cursor
         self.db_cursor.execute(
             "SELECT EXISTS (SELECT relname FROM pg_class WHERE relname=%s);", (table_name,)
         )
@@ -80,21 +101,31 @@ class DataBase:
             return True
         return False
 
-    def create_table(self):
+    def create_table(self, db_name: str, table_name: str) -> None:
+        """Create a table in database `db_name` if doesn't already exist."""
         if self.db_cursor is None:
-            self.connect_to_db("names_db")
-        if self.table_exists("names"):
+            self.connect_to_db(db_name)
+        if self._table_exists(table_name):
             return
-        self.db_cursor.execute("CREATE TABLE names (id serial PRIMARY KEY, data varchar);")
+        self.db_cursor.execute(
+            sql.SQL("CREATE TABLE {} (id serial PRIMARY KEY, data varchar);").format(
+                sql.Identifier(table_name)
+            )
+        )
+        self.db_conn.commit()
+        logger.info(f"Table '{table_name}' was created in DB '{db_name}'")
+
+    def add_name(self, name: str, db_name: str, table_name: str) -> None:
+
+        self.create_table(db_name, table_name)
+
+        self.db_cursor.execute(
+            sql.SQL("INSERT INTO {} (data) VALUES (%s);").format(sql.Identifier(table_name)),
+            (name,),
+        )
         self.db_conn.commit()
 
-    def add_name(self, name):
-        self.create_table()
-        self.db_cursor.execute("INSERT INTO names (data) VALUES (%s)", (name,))
-        self.db_conn.commit()
-
-    @property
-    def all_names(self):
-        self.create_table()
-        self.db_cursor.execute("SELECT * FROM names;")
+    def all_names(self, db_name: str, table_name: str) -> List[Tuple[int, str]]:
+        self.create_table(db_name, table_name)
+        self.db_cursor.execute(sql.SQL("SELECT * FROM {}").format(sql.Identifier(table_name)))
         return self.db_cursor.fetchall()
